c143d7925b75317a6f3bf68ca453c2f160c22b47
Marcio Curvello <marciocurvello@take.net>
2023-05-20 13:23:03 -0300

    add restaurant info card

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
+const Title = styled.Text`
+  padding: 16px;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2023/04/fresh-vegetables-on-a-white-background.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
