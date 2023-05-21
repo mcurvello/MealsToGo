f471c00a153974f149dc5fb090c414ee875daa87
Marcio Curvello <marciocurvello@take.net>
2023-05-21 17:17:31 -0300

    add tab bar

--- App.js
+++ App.js
@@ -10,7 +10,34 @@ import {
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { Text } from "react-native";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+};
+const MapScreen = () => {
+  return (
+    <SafeArea>
+      <Text>Map</Text>
+    </SafeArea>
+  );
+};
+
+const SettingsScreen = () => {
+  return (
+    <SafeArea>
+      <Text>Settings</Text>
+    </SafeArea>
+  );
+};
 export default function App() {
   const [oswaldLoaded] = useOswald({
     Oswald_400Regular,
@@ -26,7 +53,51 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "gray",
+            }}
+          >
+            <Tab.Screen
+              name="Restaurants"
+              component={RestaurantsScreen}
+              options={{
+                tabBarLabel: "Home",
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons
+                    name={TAB_ICON.Restaurants}
+                    color={color}
+                    size={size}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Map"
+              component={MapScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons name={TAB_ICON.Map} color={color} size={size} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Settings"
+              component={SettingsScreen}
+              options={{
+                tabBarIcon: ({ color, size }) => (
+                  <Ionicons
+                    name={TAB_ICON.Settings}
+                    color={color}
+                    size={size}
+                  />
+                ),
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <StatusBar style="auto" />
     </>
