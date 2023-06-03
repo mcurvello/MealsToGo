c8eccfa0b8a9e36429ea4524afd89752498d5d6d
Marcio Curvello <marciocurvello@gmail.com>
2023-06-03 14:11:52 -0300

    add camera feature

--- src/features/settings/screens/settings.screen.js
+++ src/features/settings/screens/settings.screen.js
@@ -5,6 +5,7 @@ import { Avatar, List } from "react-native-paper";
 import styled from "styled-components/native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
+import { TouchableOpacity } from "react-native";
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -19,11 +20,13 @@ export const SettingsScreen = ({ navigation }) => {
   return (
     <SafeArea>
       <AvatarContainer>
-        <Avatar.Icon
-          size={180}
-          icon="human"
-          style={{ backgroundColor: "#2182BD" }}
-        />
+        <TouchableOpacity onPress={() => navigation.navigate("Camera")}>
+          <Avatar.Icon
+            size={180}
+            icon="human"
+            style={{ backgroundColor: "#2182BD" }}
+          />
+        </TouchableOpacity>
         <Spacer position="top" size="large">
           <Text variant="label">{user.email}</Text>
         </Spacer>
